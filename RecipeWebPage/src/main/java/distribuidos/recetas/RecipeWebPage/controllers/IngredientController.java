c0ef992feb025e3331830770ae0217a86c76d8ab
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2024-04-17 12:26:49 +0200

    Customized error screen for trying to use an ingredient that is in use

--- RecipeWebPage/src/main/java/distribuidos/recetas/RecipeWebPage/controllers/IngredientController.java
+++ RecipeWebPage/src/main/java/distribuidos/recetas/RecipeWebPage/controllers/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collection;
 
@@ -67,10 +68,17 @@ public class IngredientController {
     }
 
     @PostMapping("/ingredient/{id}/delete")
-    public String deleteIngredient(@PathVariable Long id){
+    public String deleteIngredient(@PathVariable Long id, RedirectAttributes redirectAttributes){
         if (ingredientService.delete(id) == null){
+            String msg = "No se puede borrar este ingrediente ya que se está usando en al menos una receta.";
+            redirectAttributes.addFlashAttribute("errorMessage",msg);
             return "redirect:/Error"; //Can't delete that ingredient, because it has associated recipes
         }
         return "redirect:/ingredients";
+    }
+
+    @GetMapping("/Error")
+    public String error(){
+        return "Error";
     }
 }
